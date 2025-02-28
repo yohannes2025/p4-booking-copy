92c9127959bfe60f6806950845693698ba1458cd
yohannes2025 <johannes.m.tekle@gmail.com>
2025-02-28 12:44:20 +0300

    feat: Update settings.py to use django allauth authentication

--- restaurant_booking/settings.py
+++ restaurant_booking/settings.py
@@ -59,6 +59,7 @@ MIDDLEWARE = [
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
     'whitenoise.middleware.WhiteNoiseMiddleware',
+    'allauth.account.middleware.AccountMiddleware'
 ]
 
 ROOT_URLCONF = 'restaurant_booking.urls'
