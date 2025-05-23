18b1a844bc6b6c57630dc90ddaf8aa39626a2c32
yohannes2025 <johannes.m.tekle@gmail.com>
2025-05-23 21:44:48 +0200

    1.1 Remove the explicit logout and login paths because allauth handles them via accounts/

--- booking_app/urls.py
+++ booking_app/urls.py
@@ -1,6 +1,5 @@
 # booking_app/urls.py
 from django.urls import path
-from django.contrib.auth import views as auth_views
 from . import views
 
 urlpatterns = [
@@ -8,11 +7,10 @@ urlpatterns = [
     path('book/', views.book_table, name='book_table'),
     path('bookings/', views.view_bookings, name='view_bookings'),
     path('cancel/<int:booking_id>/', views.cancel_booking, name='cancel_booking'),
-    path('register/', views.register, name='register'),  # Add registration URL
-    path('logout/', auth_views.LogoutView.as_view(), name='logout'),
-    path('login/', auth_views.LoginView.as_view(), name='login'),
-    path('password_reset/', auth_views.PasswordResetView.as_view(),
-         name='password_reset'),
-    path('password_reset/done/', auth_views.PasswordResetDoneView.as_view(),
-         name='password_reset_done'),
+
+    path('register/', views.register, name='register'),
+
+    path('api/get_tables/<int:restaurant_id>/',
+         views.get_tables_for_restaurant, name='get_tables_for_restaurant'),
+
 ]
