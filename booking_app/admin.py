0b4fb712821dd4df98b1429d251501aafcf1de19
yohannes2025 <johannes.m.tekle@gmail.com>
2025-05-24 16:12:52 +0200

    1.3 Add to admin.py 'Admin Interface' for Data Management to allow administors easily manage records

--- booking_app/admin.py
+++ booking_app/admin.py
@@ -1,6 +1,25 @@
 from django.contrib import admin
 from .models import Restaurant, Table, Booking
 
-admin.site.register(Restaurant)
-admin.site.register(Table)
-admin.site.register(Booking)
+
+@admin.register(Restaurant)
+class RestaurantAdmin(admin.ModelAdmin):
+    list_display = ('name', 'address',)
+    search_fields = ('name', 'address',)
+
+
+@admin.register(Table)
+class TableAdmin(admin.ModelAdmin):
+    list_display = ('restaurant', 'table_number', 'capacity',)
+    list_filter = ('restaurant',)
+    search_fields = ('table_number',)
+
+
+@admin.register(Booking)
+class BookingAdmin(admin.ModelAdmin):
+    list_display = ('user', 'restaurant', 'table', 'booking_date',
+                    'booking_time', 'number_of_guests', 'created_at',)
+    list_filter = ('restaurant', 'booking_date', 'booking_time',)
+    search_fields = ('user__username', 'restaurant__name',
+                     'table__table_number',)
+    date_hierarchy = 'booking_date'
